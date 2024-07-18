29a51c5672c6666884d58c95ff8493e700c27a73
Kooreis <kooreis.official@gmail.com>
2024-07-18 01:40:02 -0500

    This update adds the 'push' method to the MinStack class. This method pushes a new element onto the 'stack' array. If the 'minStack' array is empty or the new element is less than or equal to the current minimum element, the new element is also pushed onto the 'minStack' array.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,6 @@
-class MinStack {
-    constructor() {
-        this.stack = [];
-        this.minStack = [];
+push(x) {
+        this.stack.push(x);
+        if (this.minStack.length === 0 || x <= this.getMin()) {
+            this.minStack.push(x);
+        }
     }
-}
